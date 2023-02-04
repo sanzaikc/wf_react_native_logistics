c540c3baf222c563ed02d8b375e3ee27077350c4
Sanjay <sanzaikc@gmail.com>
2023-02-04 19:08:41 +0545

    mend

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
+import Constants from "expo-constants";
 
 import QRSearch from "../components/home/QRSearch";
 import RecentShipping from "../components/home/RecentShipping";
@@ -8,7 +9,7 @@ import UserInfo from "../components/home/UserInfo";
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar hidden />
+      <StatusBar auto />
 
       <ScrollView>
         <UserInfo />
@@ -27,5 +28,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    paddingTop: Constants.statusBarHeight,
   },
 });
